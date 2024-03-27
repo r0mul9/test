d066e77c1e895f03647c87a066d97e4625296eb4
romul9 <broo.bro@mail.ru>
2024-03-27 23:00:52 +0300

    secont commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // At (10;10) fuel=100
         Tank anywareTank = new Tank(10, 10);
         // At (20;30) fuel=200
-        Tank customTank = new Tank(20, 30, 200);
+        Tank customTank = new Tank(30, 30, 200);
         justTank.goForward(200);
         justTank.printPosition();
         anywareTank.goBackward(-200);
